0bf04efbacde4fbe0c6ae2a4010d5f13e0d91a05
Gary Bunofsky <garybunofsky@gmail.com>
2015-04-01 20:10:45 -0400

    Added fonticon.

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -4,7 +4,7 @@
 		<div class="col-md-8 col-md-offset-2">
 			<div class="colophon">
 				<h6>Colophon</h6>
-				<p class="sm">Designed, developed and maintained by <a href="http://garybunofsky.com">Gary Bunofsky</a>. Hosted courtesy of <a href="http://mediatemple.net">Media Temple</a>. Type served by <a href="https://typekit.com" target="_blank">Typekit</a>. Valid <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fgarybunofsky.com%2F&amp;charset=%28detect+automatically%29&amp;doctype=Inline&amp;group=0&amp;user-agent=W3C_Validator%2F1.3+http%3A%2F%2Fvalidator.w3.org%2Fservices" target="_blank">HTML</a> &amp; <a href="http://jigsaw.w3.org/css-validator/validator?uri=garybunofsky.com%2Fwp-content%2Fthemes%2Froots-master%2Fassets%2Fcss%2Fapp.css&amp;profile=css3&amp;usermedium=all&amp;warning=1&amp;vextwarning=&amp;lang=en" target="_blank">CSS</a> compiled with <a href="http://gulpjs.com/">Gulp</a>. View the <a href="style-guide">style guide</a>. Check out the <a href="https://github.com/garybunofsky/garybunofsky.com">source</a>.</p>
+				<p class="sm">Designed, developed and maintained by <a href="http://garybunofsky.com">Gary Bunofsky</a>. Hosted courtesy of <a href="http://mediatemple.net">Media Temple</a>. Type served by <a href="https://typekit.com" target="_blank">Typekit</a>. Valid <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fgarybunofsky.com%2F&charset=%28detect+automatically%29&doctype=Inline&group=0&user-agent=W3C_Validator%2F1.3+http%3A%2F%2Fvalidator.w3.org%2Fservices" target="_blank">HTML</a>. Compiled with <a href="http://gulpjs.com/">Gulp</a>. View the <a href="style-guide">style guide</a>. Check out the <a href="https://github.com/garybunofsky/garybunofsky.com">source</a>.</p>
 			</div>
 		</div>
 	</div>
