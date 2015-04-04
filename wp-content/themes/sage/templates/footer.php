5e680145e6f02f1c78286701ed0c6ea3e9a771bd
Gary Bunofsky <garybunofsky@gmail.com>
2015-04-04 10:55:07 -0400

    Revised mixins. Edited colors.

--- wp-content/themes/sage/templates/footer.php
+++ wp-content/themes/sage/templates/footer.php
@@ -4,14 +4,14 @@
 		<div class="col-md-8 col-md-offset-2">
 			<div class="colophon">
 				<h6>Colophon</h6>
-				<p class="sm">Designed, developed and maintained by <a href="http://garybunofsky.com">Gary Bunofsky</a>. Hosted courtesy of <a href="http://mediatemple.net">Media Temple</a>. Type served by <a href="https://typekit.com" target="_blank">Typekit</a>. Valid <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fgarybunofsky.com%2F&charset=%28detect+automatically%29&doctype=Inline&group=0&user-agent=W3C_Validator%2F1.3+http%3A%2F%2Fvalidator.w3.org%2Fservices" target="_blank">HTML</a>. Compiled with <a href="http://gulpjs.com/">Gulp</a>. View the <a href="style-guide">style guide</a>. Check out the <a href="https://github.com/garybunofsky/garybunofsky.com">source</a>.</p>
+				<p class="sm">Designed, developed and maintained by <a href="http://garybunofsky.com">Gary Bunofsky</a>. Hosted courtesy of <a href="http://mediatemple.net">Media Temple</a>. Type served by <a href="https://typekit.com" target="_blank">Typekit</a>. Valid <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fgarybunofsky.com%2F&charset=%28detect+automatically%29&doctype=Inline&group=0&user-agent=W3C_Validator%2F1.3+http%3A%2F%2Fvalidator.w3.org%2Fservices" target="_blank">HTML</a>. Compiled with <a href="http://gulpjs.com/">Gulp</a>. Check out the <a href="style-guide">style guide</a>, view the <a href="https://github.com/garybunofsky/garybunofsky.com">source</a> or review the <a href="http://garybunofsky.com/privacy-policy/">privacy policy</a>.</p>
 			</div>
 		</div>
 	</div>
   	<div class="row center">
 			<div class="col-md-8 col-md-offset-2">
 				<div class="legal">
-						<p class="sm">&copy; <?php echo date("Y"); ?> <a href="/">Gary Bunofsky</a>, unless otherwise noted. <a href="http://garybunofsky.com/privacy-policy/">Privacy Policy</a></p>
+						<p class="sm">&copy; <?php echo date("Y"); ?> Gary Bunofsky, unless otherwise noted.</p>
 					</div>
 				</div>
 			</div>
