ab67f24f8b5d59ba01a266e63b8cba5793f3a71b
Gary Bunofsky <garybunofsky@gmail.com>
2015-05-11 19:05:59 -0400

    Renamed sage theme.

---
+++ wp-content/themes/garybunofsky/templates/footer.php
@@ -0,0 +1,19 @@
+<footer>
+  <div class="container">
+  	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="colophon">
+				<h6>Colophon</h6>
+				<p class="sm">Designed, developed and maintained by <a href="/">Gary Bunofsky</a>. Hosted courtesy of <a href="http://hosting.webmachine.io">Web Machine</a>. Type served by <a href="https://typekit.com">Typekit</a>. Valid <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fgarybunofsky.com%2F&amp;charset=%28detect+automatically%29&amp;doctype=Inline&amp;group=0&amp;user-agent=W3C_Validator%2F1.3+http%3A%2F%2Fvalidator.w3.org%2Fservices">html</a>. Compiled with <a href="http://gulpjs.com/">Gulp</a>. Check out the <a href="/style-guide/">style guide</a>, view the <a href="https://github.com/garybunofsky/garybunofsky.com">source</a> or read the <a href="/privacy-policy/">privacy policy</a>.</p>
+			</div>
+		</div>
+	</div>
+  	<div class="row center">
+			<div class="col-md-8 col-md-offset-2">
+				<div class="legal">
+						<p class="sm">&copy; <?php echo date("Y"); ?> Gary Bunofsky, unless otherwise noted.</p>
+					</div>
+				</div>
+			</div>
+  	</div>
+</footer>
