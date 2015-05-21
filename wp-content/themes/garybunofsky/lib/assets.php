af4118aa82eab86d33f3d675b68171265debec81
Gary Bunofsky <garybunofsky@gmail.com>
2015-05-20 21:23:04 -0400

    Edited Animate.css enqueue path.

--- wp-content/themes/garybunofsky/lib/assets.php
+++ wp-content/themes/garybunofsky/lib/assets.php
@@ -125,7 +125,7 @@ function assets() {
 
   // aftermarket enqueues
   wp_enqueue_style( 'fontawesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
-  wp_enqueue_style( 'animate', '//garybunofsky.com/wp-content/themes/sage/bower_components/animate.css/animate.min.css');
+  wp_enqueue_style( 'animate', '//garybunofsky.com/wp-content/themes/garybunofsky/bower_components/animate.css/animate.min.css');
   wp_enqueue_style( 'typekit', '//use.typekit.net/tnp6jhg.js');
 
 }
