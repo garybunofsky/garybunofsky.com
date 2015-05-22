e288e8bc2712deda7ea623deeb0a6ed2782ca3d3
Gary Bunofsky <garybunofsky@gmail.com>
2015-05-22 16:25:38 -0400

    removed emoji script

--- wp-content/themes/garybunofsky/functions.php
+++ wp-content/themes/garybunofsky/functions.php
@@ -30,3 +30,8 @@ foreach ($sage_includes as $file) {
   require_once $filepath;
 }
 unset($file, $filepath);
+
+
+// remove emoji script
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
