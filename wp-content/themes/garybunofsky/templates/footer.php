522d9e6730f612da9ee240844af2f0d601fbba5e
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-24 17:25:06 -0400

    Revised colophon copy.

--- wp-content/themes/garybunofsky/templates/footer.php
+++ wp-content/themes/garybunofsky/templates/footer.php
@@ -4,7 +4,7 @@
 		<div class="col-md-12 col-md-offset-6">
 			<div class="colophon">
 				<h6>Colophon</h6>
-				<p class="sm">Designed, developed and maintained by <a href="<?php echo site_url(); ?>/">Gary Bunofsky</a>. Hosted courtesy of <a href="http://hosting.webmachine.io">Web Machine</a>. Type served by <a href="https://typekit.com">Typekit</a>. Check out the <a href="<?php echo site_url(); ?>/style-guide/">style guide</a>, view the <a href="https://github.com/garybunofsky/garybunofsky.com">source</a> or read the <a href="<?php echo site_url(); ?>/privacy-policy/">privacy policy</a>.</p>
+				<p class="sm">Designed, developed and maintained by <a href="<?php echo site_url(); ?>/">Gary Bunofsky</a>. Hosted courtesy of <a href="http://hosting.webmachine.io">Web Machine</a>. Check out the <a href="<?php echo site_url(); ?>/style-guide/">style guide</a>, view the <a href="https://github.com/garybunofsky/garybunofsky.com">source</a> or read the <a href="<?php echo site_url(); ?>/privacy-policy/">privacy policy</a>.</p>
 			</div>
 		</div>
 	</div>
