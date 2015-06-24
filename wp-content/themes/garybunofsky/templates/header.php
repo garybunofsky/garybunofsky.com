699d2214d33616947e1dd81cbeba3c586602ba24
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-24 17:25:33 -0400

    Placed h2.site_title above nav.

--- wp-content/themes/garybunofsky/templates/header.php
+++ wp-content/themes/garybunofsky/templates/header.php
@@ -1,25 +1,25 @@
 <?php use Roots\Sage\Nav; ?>
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-12 col-md-offset-6">
-        <h2 class="site_title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Gary Bunofsky, Designer &amp; Developer</a></h2>
-      </div>
-    </div>
-  </div>
-
-  <div class="container">
-    <div class="row">
+<div class="container">
+    <!--div class="row">
       <div class="col-md-12 col-md-offset-6">
         <hr class="fullW"/>
       </div>
-    </div>
+    </div-->
     <div class="row">
       <div class="col-md-12 col-md-offset-6">
+        <h2 class="animated fadeIn site_title"><a href="<?php echo esc_url( home_url( '/' ) );?>">Gary Bunofsky, Designer &amp; Developer</a></h1>
         <div class="nav">
           <?php wp_nav_menu( array('menu' => 'Main Menu' )); ?>
         </div>
       </div>
     </div>
   </div>
+  <!--div class="container">
+    <div class="row">
+      <div class="col-md-12 col-md-offset-6">
+        <h1 class="site_title"><a href="< ?php echo esc_url( home_url( '/' ) ); ?>">Gary Bunofsky, Designer &amp; Developer</a></h2>
+      </div>
+    </div>
+  </div-->
 </header>
