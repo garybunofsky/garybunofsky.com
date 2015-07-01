f55a5ea9794f9ae63c9c93bb860e4e5508dbae63
Gary Bunofsky <garybunofsky@gmail.com>
2015-06-30 20:41:31 -0400

    Styled search page.

--- wp-content/themes/garybunofsky/index.php
+++ wp-content/themes/garybunofsky/index.php
@@ -1,14 +1,20 @@
-<?php get_template_part('templates/page', 'header'); ?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-10 col-md-offset-7">
+				<h1 class="animated fadeIn"><?php get_template_part('templates/page', 'header'); ?></h1>
+				<div class="mT2">
+					<?php if (!have_posts()) : ?>
+					    <?php _e('<p>Sorry, no results were found.</p>', 'sage'); ?>
+					  <?php get_search_form(); ?>
+					<?php endif; ?>
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'sage'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
+					<?php while (have_posts()) : the_post(); ?>
+					  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
+					  <hr />
+					<?php endwhile; ?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', get_post_type() != 'post' ? get_post_type() : get_post_format()); ?>
-<?php endwhile; ?>
-
-<?php the_posts_navigation(); ?>
+					<?php the_posts_navigation(); ?>
+				</div>
+			</div>
+		</div>
+	</div>
