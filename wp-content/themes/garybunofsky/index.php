bdd805a8b9fcaff0a6b7fe559b9c0999610abc1e
Gary Bunofsky <garybunofsky@gmail.com>
2015-07-01 18:25:10 -0400

    Added copy to Search Results page.

--- wp-content/themes/garybunofsky/index.php
+++ wp-content/themes/garybunofsky/index.php
@@ -1,10 +1,18 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-10 col-md-offset-7">
-				<h1 class="animated fadeIn"><?php get_template_part('templates/page', 'header'); ?></h1>
+				<h1 class="animated fadeIn">Search Results</h1>
 				<div class="mT2">
 					<?php if (!have_posts()) : ?>
-					    <?php _e('<p>Sorry, no results were found.</p>', 'sage'); ?>
+					    <?php _e('
+					    		<p>It does\'t look like we can find what you\'re looking for. You might want to:</p>
+					    		<ul>
+					    			<li>Try a different spelling</li>
+					    			<li>Use fewer words</li>
+					    			<li>Return to the <a href="/">homepage</a></li>
+					    			<li>Send me an <a href="mailto:gary@garybunofsky.com?subject=Can%20not%20find">email</a></li>
+					    		</ul>
+					    ', 'sage'); ?>
 					  <?php get_search_form(); ?>
 					<?php endif; ?>
 
