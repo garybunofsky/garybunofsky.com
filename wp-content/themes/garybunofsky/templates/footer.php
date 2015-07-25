f2642e56a746048e52134133a3caf09d06784366
Gary Bunofsky <garybunofsky@gmail.com>
2015-07-24 21:09:27 -0400

    Updated Wordpress and Yoast SEO.

--- wp-content/themes/garybunofsky/templates/footer.php
+++ wp-content/themes/garybunofsky/templates/footer.php
@@ -1,19 +1,19 @@
 <footer>
   <div class="container">
   	<div class="row">
-		<div class="col-md-10 col-md-offset-7">
-			<div class="colophon">
-				<h6>Colophon</h6>
-				<p class="sm">Designed, developed and maintained by <a href="<?php echo site_url(); ?>/">Gary Bunofsky</a>. Hosted courtesy of <a href="http://hosting.webmachine.io">Web Machine</a>. Check out the <a href="<?php echo site_url(); ?>/style-guide/">style guide</a>, view the <a href="https://github.com/garybunofsky/garybunofsky.com">source</a> or read the <a href="<?php echo site_url(); ?>/privacy-policy/">privacy policy</a>.</p>
+			<div class="col-md-10 col-md-offset-7">
+				<div class="colophon">
+					<h6>Colophon</h6>
+					<p class="sm">Designed, developed and maintained by <a href="<?php echo site_url(); ?>/">Gary Bunofsky</a>. Hosted courtesy of <a href="http://hosting.webmachine.io">Web Machine</a>. Check out the <a href="<?php echo site_url(); ?>/style-guide/">style guide</a>, view the <a href="https://github.com/garybunofsky/garybunofsky.com">source</a> or read the <a href="<?php echo site_url(); ?>/privacy-policy/">privacy policy</a>.</p>
+				</div>
 			</div>
 		</div>
-	</div>
   	<div class="row center">
 			<div class="col-md-10 col-md-offset-7">
 				<div class="legal">
-						<p class="sm">&copy; <?php echo date("Y"); ?> Gary Bunofsky, unless otherwise noted.</p>
-					</div>
+					<p class="sm">&copy; <?php echo date("Y"); ?> Gary Bunofsky, unless otherwise noted.</p>
 				</div>
 			</div>
-  	</div>
+		</div>
+  </div>
 </footer>
