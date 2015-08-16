cb9d96dad997e13d85f734be9b8167deb15186d3
Gary Bunofsky <garybunofsky@gmail.com>
2015-08-15 22:07:57 -0400

    Replaced footer colophon with MC newsletter form.

--- wp-content/themes/garybunofsky/templates/footer.php
+++ wp-content/themes/garybunofsky/templates/footer.php
@@ -3,17 +3,56 @@
   	<div class="row">
 			<div class="col-md-10 col-md-offset-7">
 				<div class="colophon">
-					<h6>Colophon</h6>
-					<p class="sm">Designed, developed and maintained by <a href="<?php echo site_url(); ?>/">Gary Bunofsky</a>. Hosted courtesy of <a href="http://hosting.webmachine.io">Web Machine</a>. Check out the <a href="<?php echo site_url(); ?>/style-guide/">style guide</a>, view the <a href="https://github.com/garybunofsky/garybunofsky.com">source</a> or read the <a href="<?php echo site_url(); ?>/privacy-policy/">privacy policy</a>.</p>
+					<!--h6>Colophon</h6-->
+					<h6>Newsletter</h6>
+					<!-- Begin MailChimp Signup Form -->
+					<div id="mc_embed_signup">
+						<p>I send out a newsletter every few weeks. It covers topics regarding design, development and tech.</p>
+						<form action="//garybunofsky.us11.list-manage.com/subscribe/post?u=deb5a86031cefe2be60767bf5&amp;id=84b423ba68" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+							<div class="row">
+								<div class="col-sm-24">
+									<label>Email *</label>
+									<input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL">
+								</div>
+							</div>
+							<!--div class="row">
+								<div class="col-sm-24">
+									<label>First Name</label>
+									<input type="text" value="" name="FNAME" class="form-control" id="mce-FNAME">
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-sm-24">
+									<label>Last Name</label>
+									<input type="text" value="" name="LNAME" class="form-control" id="mce-LNAME">
+								</div>
+							</div-->
+					    <div style="position: absolute; left: -5000px;"><input type="text" name="b_deb5a86031cefe2be60767bf5_84b423ba68" tabindex="-1" value=""></div>
+					    	<div class="row">
+					    		<div class="col-sm-8">
+					    			<input type="submit" value="Subscribe" name="subscribe" class="btn">
+					    		</div>
+					    	</div>
+					    	<div id="mce-responses" class="clear">
+								<div class="response" id="mce-error-response" style="display:none"></div>
+								<div class="response" id="mce-success-response" style="display:none"></div>
+							</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					    </div>
+						</form>
+					</div>
+					<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+					<!--End mc_embed_signup-->
 				</div>
 			</div>
 		</div>
-  	<div class="row center">
+		</div>
+		<div class="container">
+		<div class="row">
 			<div class="col-md-10 col-md-offset-7">
 				<div class="legal">
-					<p class="sm">&copy; <?php echo date("Y"); ?> Gary Bunofsky, unless otherwise noted.</p>
+					<p class="sm">&copy; <?php echo date("Y"); ?> <a href="/">Gary Bunofsky</a> / <a href="style-guide">Style Guide</a> / <a href="privacy-policy">Privacy Policy</a></p>
 				</div>
 			</div>
 		</div>
-  </div>
+	</div>
 </footer>
