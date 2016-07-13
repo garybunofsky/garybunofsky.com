f80d090cede21247065bb78656bcf54d2d3a121d
garybunofsky <garybunofsky@gmail.com>
2016-07-13 19:38:32 -0400

    Created graph for changelog and added d3 gem.

--- app/controllers/changelog_controller.rb
+++ app/controllers/changelog_controller.rb
@@ -4,5 +4,12 @@ class ChangelogController < ApplicationController
 		uri = URI(request_uri)
 		response = Net::HTTP.get(uri)
 		@data = JSON.parse(response)
-  end
+
+		@month_array = Array.new
+		@data.each do |item|
+				 @commit_month = Date.parse(item['commit']['author']['date']).strftime('%Y-%m-%d')
+				 @month_array.push @commit_month
+		 end
+		 @counts = @month_array.reverse.group_by{|i| i}.map{|k,v| [k, v.count] }.to_json.html_safe
+	end
 end
