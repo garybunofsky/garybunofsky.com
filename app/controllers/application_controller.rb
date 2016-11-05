567413f17b4c57d1edea6c352a0bff65b63c8b8f
garybunofsky <garybunofsky@gmail.com>
2016-11-05 09:16:30 -0400

    Redirect www to apex domain.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :redirect_subdomain
+  def redirect_subdomain
+    if request.host == 'www.garybunofsky.com'
+      redirect_to 'https://garybunofsky.com' + request.fullpath, :status => 301
+    end
+  end
+  
 end
