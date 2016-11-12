9463a489d04a3dfc9c48381136f00be541208675
garybunofsky <garybunofsky@gmail.com>
2016-11-12 14:17:23 -0500

    Added new site map for about page.

--- sitemap.rb
+++ sitemap.rb
@@ -4,6 +4,7 @@ require 'sitemap_generator'
 SitemapGenerator::Sitemap.default_host = 'https://garybunofsky.com'
 SitemapGenerator::Sitemap.create do
   add '/home', :changefreq => 'weekly', :priority => 0.9
+  add '/about', :changefreq => 'monthly', :priority => 0.5
   add '/blog', :changefreq => 'weekly', :priority => 0.7
 	add '/blog/basic-git-commands', :changefreq => 'weekly', :priority => 0.6
 	add '/blog/finding-photos', :changefreq => 'weekly', :priority => 0.6
@@ -12,7 +13,7 @@ SitemapGenerator::Sitemap.create do
 	add '/changelog', :changefreq => 'weekly', :priority => 0.4
 	add '/portfolio', :changefreq => 'weekly', :priority => 0.8
 	add '/libary', :changefreq => 'weekly', :priority => 0.5
-	add '/resume', :changefreq => 'weekly', :priority => 0.9
+	add '/Gary-Bunofsky-Resume', :changefreq => 'weekly', :priority => 0.9
 	add '/style-guide', :changefreq => 'weekly', :priority => 0.3
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
