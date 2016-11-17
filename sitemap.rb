16f4e5cf23a23a0b397d100b99435eea823fa69a
garybunofsky <garybunofsky@gmail.com>
2016-11-17 06:05:18 -0500

    Added new blog post and updated old blog post.

--- sitemap.rb
+++ sitemap.rb
@@ -5,11 +5,12 @@ SitemapGenerator::Sitemap.default_host = 'https://garybunofsky.com'
 SitemapGenerator::Sitemap.create do
   add '/home', :changefreq => 'weekly', :priority => 0.9
   add '/about', :changefreq => 'monthly', :priority => 0.5
-  add '/blog', :changefreq => 'weekly', :priority => 0.7
-	add '/blog/basic-git-commands', :changefreq => 'weekly', :priority => 0.6
-	add '/blog/finding-photos', :changefreq => 'weekly', :priority => 0.6
-	add '/blog/the-hamburger-icon', :changefreq => 'weekly', :priority => 0.6
-	add '/blog/website-deployment-checklist', :changefreq => 'weekly', :priority => 0.6
+  add '/blog', :changefreq => 'monthly', :priority => 0.7
+  add '/blog/not-the-same', :changefreq => 'monthly', :priority => 0.6
+	add '/blog/basic-git-commands', :changefreq => 'monthly', :priority => 0.6
+	add '/blog/finding-photos', :changefreq => 'monthly', :priority => 0.6
+	add '/blog/the-hamburger-icon', :changefreq => 'monthly', :priority => 0.6
+	add '/blog/website-deployment-checklist', :changefreq => 'monthly', :priority => 0.6
 	add '/changelog', :changefreq => 'weekly', :priority => 0.4
 	add '/portfolio', :changefreq => 'weekly', :priority => 0.8
 	add '/libary', :changefreq => 'weekly', :priority => 0.5
@@ -17,4 +18,3 @@ SitemapGenerator::Sitemap.create do
 	add '/style-guide', :changefreq => 'weekly', :priority => 0.3
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
-d 
