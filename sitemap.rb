be35afc1effb9f978a50b4fb91dea8630d553404
garybunofsky <garybunofsky@gmail.com>
2016-12-04 14:52:31 -0500

    New sitemap.

--- sitemap.rb
+++ sitemap.rb
@@ -3,7 +3,7 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'https://garybunofsky.com'
 SitemapGenerator::Sitemap.create do
-  add '/home', :changefreq => 'weekly', :priority => 0.9
+  add '/', :changefreq => 'weekly', :priority => 0.9
   add '/about', :changefreq => 'monthly', :priority => 0.5
   add '/blog', :changefreq => 'monthly', :priority => 0.7
   add '/blog/facebooks-new-job', :changefreq => 'monthly', :priority => 0.6
@@ -14,6 +14,7 @@ SitemapGenerator::Sitemap.create do
 	add '/blog/website-deployment-checklist', :changefreq => 'monthly', :priority => 0.6
 	add '/changelog', :changefreq => 'weekly', :priority => 0.4
 	add '/portfolio', :changefreq => 'weekly', :priority => 0.8
+  add '/portfolio/magenda', :changefreq => 'weekly', :priority => 0.8
 	add '/library', :changefreq => 'weekly', :priority => 0.5
 	add '/resume.pdf', :changefreq => 'weekly', :priority => 0.9
 	add '/style-guide', :changefreq => 'weekly', :priority => 0.3
