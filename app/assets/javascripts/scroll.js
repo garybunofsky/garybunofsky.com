a989c92bab330a97ce21a522f16bce11d03c9446
garybunofsky <garybunofsky@gmail.com>
2017-02-09 16:47:33 -0500

    Updated js to add shadow on scroll.

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,22 @@
+$(function() {
+  initPage();
+});
+
+$(window).bind('page:change', function() {
+  initPage();
+});
+
+function initPage() {
+  $(function() {
+      var nav = $("nav");
+      $(window).scroll(function() {    
+          var scroll = $(window).scrollTop();
+          if (scroll > 0) {
+              nav.addClass("shadow");
+          } else {
+              nav.removeClass("shadow");
+          }
+      });
+  });
+}
+
