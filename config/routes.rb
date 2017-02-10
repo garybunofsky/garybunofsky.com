7fcf534582fc875e180ce666e71ef2b9df2e9650
garybunofsky <garybunofsky@gmail.com>
2017-02-10 16:57:13 -0500

    Created 404 and 500 error pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-
-	root 'root#index'
-
+  
+	root 'root#index'	
 	controller :changelog do
 		get '/changelog' => :index
 	end
-
+  
+  controller :errors do
+    match "/404", :to => :not_found, :via => :all
+    match "/500", :to => :internal_server_error, :via => :all
+  end
+  
 	controller :static do
 		get '/:page' => :index, :as => :static
 		get '/:page/:subpage' => :show, :as => :subpage
