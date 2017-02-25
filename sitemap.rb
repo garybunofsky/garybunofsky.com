9b63bf90d8b671b264777f88ada04567415b0eec
garybunofsky <garybunofsky@gmail.com>
2017-02-25 15:43:44 -0500

    Updated sitemap with 404 page.

--- sitemap.rb
+++ sitemap.rb
@@ -3,24 +3,25 @@ require 'sitemap_generator'
 
 SitemapGenerator::Sitemap.default_host = 'https://garybunofsky.com'
 SitemapGenerator::Sitemap.create do
-  add '/', :changefreq => 'monthly', :priority => 0.9
-  add '/about', :changefreq => 'monthly', :priority => 0.5
-  add '/blog', :changefreq => 'monthly', :priority => 0.7
-  add '/blog/facebooks-new-job', :changefreq => 'monthly', :priority => 0.6
-  add '/blog/not-the-same', :changefreq => 'monthly', :priority => 0.6
-	add '/blog/basic-git-commands', :changefreq => 'monthly', :priority => 0.6
-	add '/blog/finding-photos', :changefreq => 'monthly', :priority => 0.6
-	add '/blog/the-hamburger-icon', :changefreq => 'monthly', :priority => 0.6
-	add '/blog/website-deployment-checklist', :changefreq => 'monthly', :priority => 0.6
-	add '/changelog', :changefreq => 'weekly', :priority => 0.1
-	add '/portfolio', :changefreq => 'weekly', :priority => 0.8
-  add '/portfolio/magenda', :changefreq => 'weekly', :priority => 0.8
-  add '/portfolio/tiny-notes', :changefreq => 'weekly', :priority => 0.8
-  add '/portfolio/veronica', :changefreq => 'weekly', :priority => 0.8
-  add '/portfolio/pass', :changefreq => 'weekly', :priority => 0.8
-	add '/library', :changefreq => 'weekly', :priority => 0.5
-	add '/resume.pdf', :changefreq => 'weekly', :priority => 0.9
-	add '/style-guide', :changefreq => 'weekly', :priority => 0.3
-  add '/pay', :changefreq => 'monthly', :priority => 0.3
+  add '/',                                     :changefreq => 'monthly', :priority => 0.9
+  add '/about',                                :changefreq => 'monthly', :priority => 0.5
+  add '/blog',                                 :changefreq => 'monthly', :priority => 0.7
+  add '/blog/facebooks-new-job',               :changefreq => 'monthly', :priority => 0.6
+  add '/blog/not-the-same',                    :changefreq => 'monthly', :priority => 0.6
+	add '/blog/basic-git-commands',              :changefreq => 'monthly', :priority => 0.6
+	add '/blog/finding-photos',                  :changefreq => 'monthly', :priority => 0.6
+	add '/blog/the-hamburger-icon',              :changefreq => 'monthly', :priority => 0.6
+	add '/blog/website-deployment-checklist',    :changefreq => 'monthly', :priority => 0.6
+	add '/changelog',                            :changefreq => 'weekly',  :priority => 0.1
+	add '/library',                              :changefreq => 'weekly',  :priority => 0.5
+  add '/pay',                                  :changefreq => 'monthly', :priority => 0.3
+	add '/portfolio',                            :changefreq => 'weekly',  :priority => 0.8
+  add '/portfolio/magenda',                    :changefreq => 'weekly',  :priority => 0.8
+  add '/portfolio/tiny-notes',                 :changefreq => 'weekly',  :priority => 0.8
+  add '/portfolio/veronica',                   :changefreq => 'weekly',  :priority => 0.8
+  add '/portfolio/pass',                       :changefreq => 'weekly',  :priority => 0.8
+	add '/resume.pdf',                           :changefreq => 'weekly',  :priority => 0.9
+	add '/style-guide',                          :changefreq => 'weekly',   :priority => 0.3
+  add '/404',                                  :changefreq => 'weekly',   :priority => 0.1
 end
 SitemapGenerator::Sitemap.ping_search_engines # Not needed if you use the rake tasks
