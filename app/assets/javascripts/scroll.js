098c5e1128827887a42197fe21c05e8a2031f1ac
garybunofsky <garybunofsky@gmail.com>
2017-04-03 18:31:01 -0400

    Removed console.logs for debugging purposes.

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,19 +1,15 @@
 $(document).on('page:change', function() {
-  console.log("page change");
   initPage();
 });
 
 function initPage() {
-  console.log("initialize page");
   $(function() {
       var nav = $("nav");
       $(window).scroll(function() {    
           var scroll = $(window).scrollTop();
           if (scroll > 0) {
-              console.log("addClass shadow");
               nav.addClass("shadow");
           } else {
-              console.log("removeClass shadow");
               nav.removeClass("shadow");
           }
       });
