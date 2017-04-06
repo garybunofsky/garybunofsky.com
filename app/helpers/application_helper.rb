9454e72aa37622c07f9ba7c3c467705629500c9a
garybunofsky <garybunofsky@gmail.com>
2017-04-06 18:20:35 -0400

    Added Hotjar script.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,7 @@
 module ApplicationHelper
+  def zoom_image(image, alt) 
+    return link_to '', data: { confirm: "#{image}", alt: "#{alt}" }
+  end
   def browser(img_src, alt, url)
     return "<div class=\"browser mb-8\">
       <div class=\"browser_header\">
@@ -7,7 +10,7 @@ module ApplicationHelper
           <li class=\"spotlights_item\"></li>
           <li class=\"spotlights_item\"></li>
         </ul>
-        <div class=\"search\">
+        <div class=\"search mx-auto\">
           <span>#{url}</span>
         </div>
       </div>
