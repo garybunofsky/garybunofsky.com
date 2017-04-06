bd1d632a26e4d82434f9b2321807d9a46f65dcdf
garybunofsky <garybunofsky@gmail.com>
2017-04-05 22:04:29 -0400

    Created browser helper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def browser(img_src, alt, url)
+    return "<div class=\"browser mb-8\">
+      <div class=\"browser_header\">
+        <ul class=\"spotlights\">
+          <li class=\"spotlights_item\"></li>
+          <li class=\"spotlights_item\"></li>
+          <li class=\"spotlights_item\"></li>
+        </ul>
+        <div class=\"search\">
+          <span>#{url}</span>
+        </div>
+      </div>
+      <img src=\"#{img_src}\" alt=\"#{alt}.\">
+    </div>".html_safe
+  end
 end
+
