5dfca784f18a501681eaa56965bc84555be235ee
Gary Bunofsky <gary@garybunofsky.com>
2018-02-06 19:47:02 -0500

    Removed all node stuff

--- app/controllers/static_controller.rb
+++
@@ -1,8 +0,0 @@
-class StaticController < ApplicationController
-  def index
-    render template: "#{params[:page]}/index.erb"
-  end
-  def show
-    render template: "#{params[:page]}/#{params[:subpage]}"
-  end
-end
