a5b8c9767e4be211e7ae23a79f31e6b6946b3b50
Gary Bunofsky <gary@garybunofsky.com>
2018-04-05 22:02:46 -0400

    Added slash before dist in index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express()
 var serveStatic = require('serve-static')
 var port = process.env.PORT || 3000
 var path = require('path')
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(serveStatic(path.join(__dirname, '/dist')))
 
 app.listen(port)
 console.log('Server started on port ' + port)
