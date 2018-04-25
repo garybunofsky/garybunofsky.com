78c9762ef302b809ebb49e4e09c6170546a60776
Gary Bunofsky <gary@garybunofsky.com>
2018-04-25 18:58:44 -0400

    Checking in blog post and new index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ var express = require('express')
 var app = express()
 var path = require('path')
 var serveStatic = require('serve-static')
-app.use(
-  serveStatic(path.join(__dirname, 'src'))
-)
+
+app.use(express.static(path.join(__dirname, 'src'),{index:false,extensions:['html']}))
 
 app.listen(3000)
 console.log('Server started on port 3000')
