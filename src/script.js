e509abe23e2236cfe5a4c368bf47d4b66f5f849b
Gary Bunofsky <gary@garybunofsky.com>
2018-04-26 18:58:30 -0400

    Updated navigation and footer

--- src/script.js
+++ src/script.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-var i = 0
-  document.getElementById('menu').addEventListener("click", function () {
-    document.getElementById('nav').classList.toggle("hide");
-    if (i % 2 !== 0) {
-      i++
-      this.innerHTML = "Menu";
-    } else {
-      i++
-      this.innerHTML = "Close Menu";
+  console.log('👋 Thanks for checkout out what\'s under the hood.');
+  console.log('This site is static HTML and CSS, served via Node Express.');
+  var i = 0
+  var menu = document.getElementById('menu')
+  menu.addEventListener("click", function () {
+    toggleMenu();
+  });
+
+  menu.addEventListener("keyup", function (e) {
+    if (e.keyCode === 13) {
+      toggleMenu();
     }
   });
+
+  function toggleMenu () {
+    document.getElementById('nav').classList.toggle("hide");
+    if (i % 2 !== 0) {
+      menu.innerHTML = "Menu";
+    } else {
+      menu.innerHTML = "❌";
+    }
+    i++
+  }
 });
