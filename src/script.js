c4dda7515f48946ab6d43718ec37cb6254aac76e
Gary Bunofsky <gary@garybunofsky.com>
2018-04-26 21:28:14 -0400

    Removed close icon

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (i % 2 !== 0) {
       menu.innerHTML = "Menu";
     } else {
-      menu.innerHTML = "❌";
+      menu.innerHTML = "Close";
     }
     i++
   }
