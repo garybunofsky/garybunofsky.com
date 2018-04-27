61041add56b31be1a3fee95219391dc5947c4e70
Gary Bunofsky <gary@garybunofsky.com>
2018-04-26 21:09:28 -0400

    New portfolio, all static.

--- index.js
+++ index.js
@@ -2,8 +2,18 @@ var express = require('express')
 var app = express()
 var path = require('path')
 var serveStatic = require('serve-static')
-
-app.use(express.static(path.join(__dirname, 'src'),{index:false,extensions:['html']}))
+app.use(
+  (req, res, next) => {
+    const test = /\?[^]*\//.test(req.url)
+    if (req.url.substr(-1) === '/' && req.url.length > 1 && !test && !req.url.includes('blog')) {
+      res.redirect(301, req.url.slice(0, -1))
+    }
+    else {
+      next();
+    }
+  },
+  express.static(__dirname + '/src/', {extensions: ['html']})
+);
 
 app.listen(3000)
 console.log('Server started on port 3000')
