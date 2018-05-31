e99e42b5829ef04728ec624099362ed84d392492
Gary Bunofsky <gary@garybunofsky.com>
2018-05-31 13:42:42 -0400

    Adjusted heading and moved portfolio about pages

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var path = require('path')
 var serveStatic = require('serve-static')
 var port = 3000
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => {
-  res.render('index')
-})
+app.get('/', (req, res) => { res.render('index') })
+app.get('/about', (req, res) => { res.render('about') })
+app.get('/portfolio', (req, res) => { res.render('portfolio') })
 app.use(express.static('public'))
 app.listen(process.env.PORT || port)
 console.log('Server started on port ' + process.env.PORT || port)
