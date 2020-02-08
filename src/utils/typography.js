ead9ad4a0b43b7db24522f8c52c1e51d1b3a1867
Gary Bunofsky <gary@garybunofsky.com>
2020-02-08 11:11:03 -0500

    Increased font size

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,7 @@
 import Typography from "typography";
 
 const typography = new Typography({
-  baseFontSize: "16px",
+  baseFontSize: "19px",
   baseLineHeight: 1.666,
   headerFontFamily: [
     "BlinkMacSystemFont",
