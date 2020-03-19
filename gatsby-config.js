46cd0b0217a6d658e66fe3e0e341c100e1fee2ad
Gary Bunofsky <gary@garybunofsky.com>
2020-03-19 16:39:30 -0400

    Removed inter from gatsby config, because we don't need it there

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,13 +26,6 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
       }
-    },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [`inter`, `source sans pro\:300,400,400i,700`],
-        display: "swap"
-      }
     }
   ]
 };
