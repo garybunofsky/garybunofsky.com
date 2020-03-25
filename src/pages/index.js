b1e0f961828695b283b5ca2f96132e3ef8ddb0ae
Gary Bunofsky <gary@garybunofsky.com>
2020-03-25 12:53:16 -0400

    Open graph

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,50 @@ import React from "react";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
+import Helmet from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-  </Layout>
-);
+const IndexPage = () => {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
+        }
+      }
+    `
+  );
 
+  return (
+    <div>
+      <Helmet>
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://garybunofsky.com/" />
+        <meta property="og:title" content={site.siteMetadata.title} />
+        <meta
+          property="og:image"
+          content="https://garybunofsky.com/open-graph-image.png"
+        />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://garybunofsky.com/" />
+        <meta property="twitter:title" content={site.siteMetadata.title} />
+        />
+        <meta
+          property="twitter:description"
+          content={site.siteMetadata.description}
+        />
+        <meta
+          property="twitter:image"
+          content="https://garybunofsky.com/open-graph-image.png"
+        />
+      </Helmet>
+      <SEO title="Home" />
+      <Layout />
+    </div>
+  );
+};
 export default IndexPage;
