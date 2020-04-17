e56005104d1758d93c433982ba097e60ee1ba1a6
Gary Bunofsky <gary@garybunofsky.com>
2020-04-16 22:36:32 -0400

    Update blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,6 +3,7 @@ import React from "react";
 import { useStaticQuery, graphql } from "gatsby";
 import { rhythm, scale } from "../utils/typography";
 import Blog from "../components/blog";
+import Drawing from "../components/drawing";
 
 const BlogPage = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -23,15 +24,29 @@ const BlogPage = ({ children }) => {
 
   return (
     <main className="container">
-      <div
-        style={{
-          maxWidth: `320px`,
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          marginBottom: rhythm(0.75),
-          opacity: `0.75`
-        }}
-      ></div>
+
+<div
+      style={{
+        maxWidth: `320px`,
+        marginLeft: `auto`,
+        marginRight: `auto`,
+        marginBottom: rhythm(0.75),
+        opacity: `0.75`
+      }}
+    >
+      <Drawing />
+    </div>
+
+    <h1
+      style={{
+        ...scale(1),
+        marginBottom: rhythm(0.5)
+      }}
+    >
+      Blog
+    </h1>
+    <p>This isn't really a blog; it's more of hodgepodge of half baked ideas, notes, scripts and other things I don't want to forget. Anything you see here is subject to change.</p>  
+
       {data.allMarkdownRemark.edges.map(post => {
         const { title, path, date } = post.node.frontmatter;
         return (
