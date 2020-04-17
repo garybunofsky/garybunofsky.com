e369aedf82960c673f718a6106e3f44c998411ca
Gary Bunofsky <gary@garybunofsky.com>
2020-04-17 18:12:20 -0400

    Updated bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -30,15 +30,25 @@ const Bio = ({ siteTitle, siteDescription }) => (
         ...scale(0)
       }}
     >
-      {siteDescription} I am currently looking for a full-time role —{" "}
+      {siteDescription} I am currently looking freelance projects —{" "}
       <a
         href="https://docs.google.com/document/d/169vwJyUUlLRKcY-VihmY2DCSJLqQmjrw2KB42Jx8G2I/edit?usp=sharing"
         target="_blank"
         rel="noopener noreferrer"
         className="highlight"
       >
-        check out my résumé.
+        check out my résumé
+      </a>{" "}
+      or{" "}
+      <a
+        href="mailto:gary@garybunofsky.com?subject=Hi%20Gary"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="highlight"
+      >
+        send me an email
       </a>
+      .
     </p>
   </div>
 );
